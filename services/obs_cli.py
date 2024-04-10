ed4ed729558b2297a40a51c2f773250bad722178
v4u6h4n <v4u6h4ny@gmail.com>
2024-04-10 17:41:56 +1000

    Features: - OBS_CLI: logging, error alerts, log files, hotkey key triggers.
    Fixes:
    - All sources update streamdeck profile pages when using profile command.
    - Startup command.
    - Automation fixes

--- services/obs_cli.py
+++ services/obs_cli.py
@@ -2,16 +2,21 @@
 # coding: utf-8
 
 import argparse
-import logging
 import re
 import os
 import obsws_python as obs
 import socket
+import subprocess
 import sys
 import time
+from loguru import logger
 from threading import Thread
 
+
+directory_audio = "../alerts/"
+directory_data = "/media/storage/Streaming/Software/data/"
 directory_socket = '/tmp/script_socket'
+
 
 def read_client(file_path):
     with open(file_path, 'r') as file:
@@ -20,8 +25,20 @@ def read_client(file_path):
         password = file.readline().strip()
     return name, port, password
 
+
 class ObsItemNotFoundException(ValueError):
     pass
+
+
+def startup_logger():
+    logger.add("/media/storage/Streaming/Software/logs/obs_cli/{time:YYYY-MM-DD}.log",  rotation="00:00", format="{time:HH:mm:ss.SSSS} | {level} | {message}", level="INFO", compression="zip")
+    logger.add(logger_error, level="ERROR", enqueue=True)
+    logger.success("Logger started.")
+
+
+def logger_error(record):
+    logger.info("Error sounds.")
+    subprocess.run(['paplay', "/media/storage/Streaming/Software/scripts/dev/alerts/debug_error.wav"])
 
 
 def switch_to_scene(client, scene, exact=False, ignorecase=True):
@@ -121,19 +138,21 @@ def is_source_enabled(client, source, scene=None, is_group=False):
     return data.get("sceneItemEnabled")
 
 
+@logger.catch(reraise=True)
 def show_source(client, source, scene=None, is_group=False):
     scene = scene or get_current_scene_name(client)
     source_id = get_source_id(client, source=source, scene=scene, is_group=is_group)
     parent = scene if is_group else get_source_parent(client, source, scene)
-    print("Show: scene: " + scene + ", source: " + source)
+    logger.info("Show: scene: " + scene + ", source: " + source)
     return client.set_scene_item_enabled(parent, source_id, True)
 
 
+@logger.catch(reraise=True)
 def hide_source(client, source, scene=None, is_group=False):
     scene = scene or get_current_scene_name(client)
     source_id = get_source_id(client, source=source, scene=scene, is_group=is_group)
     parent = scene if is_group else get_source_parent(client, source, scene)
-    print("Hide: scene: " + scene + ", source: " + source)
+    logger.info("Hide: scene: " + scene + ", source: " + source)
     return client.set_scene_item_enabled(parent, source_id, False)
 
 
@@ -171,116 +190,94 @@ def set_input_setting(client, input, key, value):
 def get_mute_state(client, input):
     return client.get_input_mute(input).input_muted
 
-
+@logger.catch(reraise=True)
 def mute_input(client, input):
-    print("Input mute: " + input)
+    logger.info("Input mute: " + input)
     client.set_input_mute(input, True)
 
-
+@logger.catch(reraise=True)
 def unmute_input(client, input):
-    print("Input unmute: " + input)
     client.set_input_mute(input, False)
-
+    logger.info("Input unmute: " + input)
 
 def toggle_mute_input(client, input):
     client.toggle_input_mute(input)
 
-
 def get_filters(client, input):
     return client.get_source_filter_list(input).filters
-
 
 def is_filter_enabled(client, source, filter):
     return client.get_source_filter(source, filter).filter_enabled
 
-
 def enable_filter(client, source, filter):
     return client.set_source_filter_enabled(source, filter, True)
 
-
 def disable_filter(client, source, filter):
     return client.set_source_filter_enabled(source, filter, False)
-
 
 def toggle_filter(client, source, filter):
     enabled = is_filter_enabled(client, source, filter)
     return client.set_source_filter_enabled(source, filter, not enabled)
 
-
 def get_hotkeys(client):
     return client.get_hot_key_list().hotkeys
-
 
 def trigger_hotkey(client, hotkey):
     return client.trigger_hot_key_by_name(hotkey)
 
-
 def virtual_camera_status(client):
     return client.get_virtual_cam_status().output_active
-
 
 def virtual_camera_start(client):
     return client.start_virtual_cam()
 
-
 def virtual_camera_stop(client):
     return client.stop_virtual_cam()
-
 
 def virtual_camera_toggle(client):
     return client.toggle_virtual_cam()
 
-
 def stream_status(client):
     return client.get_stream_status().output_active
-
 
 def stream_start(client):
     return client.start_stream()
 
-
 def stream_stop(client):
     return client.stop_stream()
-
 
 def stream_toggle(client):
     return client.toggle_stream()
 
-
 def replay_start(client):
     return client.start_replay_buffer()
-
 
 def replay_stop(client):
     return client.stop_replay_buffer()
 
-
 def replay_save(client):
     return client.save_replay_buffer()
-
 
 def replay_toggle(client):
     return client.toggle_replay_buffer()
 
-
 def replay_status(client):
     return client.get_replay_buffer_status().output_active
-
 
 def record_status(client):
     return client.get_record_status().output_active
 
-
 def record_start(client):
     return client.start_record()
-
 
 def record_stop(client):
     return client.stop_record()
 
-
 def record_toggle(client):
     return client.toggle_record()
+
+def hotkey_trigger_key(client, key):
+    return client.trigger_hot_key_by_key_sequence(key, pressShift=False, pressCtrl=False, pressAlt=False, pressCmd=False)
 
 
 def handle_command(command, clients):
@@ -299,6 +296,20 @@ def handle_command(command, clients):
         source_parser.add_argument('subcommand', choices=['hide', 'show'], help='Subcommand')
         source_parser.add_argument('scene', type=str, help='Scene name.')
         source_parser.add_argument('source', type=str, help='Source name.')
+
+        hotkey_parser = subparsers.add_parser('hotkey', help='Hotkey subcommand.')
+        hotkey_subparsers = hotkey_parser.add_subparsers(dest='subcommand', help='Subcommand')
+
+        hotkey_list_parser = hotkey_subparsers.add_parser('list', help='List all hotkeys')
+
+        hotkey_trigger_parser = hotkey_subparsers.add_parser('trigger', help='Trigger a specific hotkey')
+        hotkey_trigger_subparsers = hotkey_trigger_parser.add_subparsers(dest='trigger_subcommand', help='Subcommand')
+
+        hotkey_trigger_name_parser = hotkey_trigger_subparsers.add_parser('name', help='Trigger hotkey by name.')
+        hotkey_trigger_name_parser.add_argument('name', type=str, help='Hotkey name.')
+
+        hotkey_trigger_key_parser = hotkey_trigger_subparsers.add_parser('key', help='Trigger hotkey by key.')
+        hotkey_trigger_key_parser.add_argument('key', type=str, help='Hotkey key.')
 
         args = parser.parse_args(command_args)
 
@@ -320,6 +331,14 @@ def handle_command(command, clients):
                     show_source(client, args.source, args.scene)
                 else:
                     print("Invalid source subcommand: " + args.subcommand)
+            elif args.operation == 'hotkey':
+                if args.subcommand == 'list':
+                    print("Not implemented.")
+                elif args.subcommand == 'trigger':
+                    if args.trigger_subcommand == 'key':
+                        hotkey_trigger_key(client, args.key)
+                else:
+                    print("Invalid hotkey subcommand: " + args.subcommand)
             else:
                 print("Invalid argument.")
                 return
@@ -329,25 +348,6 @@ def handle_command(command, clients):
 
     except Exception as e:
         print(f"Error: {e}")
-
-# def command_listener(clients):
-#     try:
-#         server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#         if os.path.exists(directory_socket):
-#             os.unlink(directory_socket)
-#         server_socket.bind(directory_socket)
-#         server_socket.listen(1)
-
-#         while True:
-#             client_socket, _ = server_socket.accept()
-#             command = client_socket.recv(1024).decode()
-#             if command:
-#                 print("Command:", command)
-#                 handle_command(command, clients)
-#             client_socket.close()
-#     except Exception as e:
-#         print(f"Error: {e}")
-#         sys.exit(1)
 
 
 def command_listener(clients):
@@ -362,31 +362,31 @@ def command_listener(clients):
             client_socket, _ = server_socket.accept()
             command = client_socket.recv(1024).decode()
             if command:
-                print("Command:", command)
+                logger.info("Command:", command)
                 handle_command(command, clients)
     except Exception as e:
         print(f"Error: {e}")
         sys.exit(1)
 
 
+@logger.catch(reraise=True)
 def main():
-    logging.basicConfig()
 
-    clients_directory = "/media/storage/Streaming/Software/data/"
+    startup_logger()
 
     try:
         client_directories = {}
 
-        for filename in os.listdir(clients_directory):
+        for filename in os.listdir(directory_data):
             if filename.startswith("obs_client_") and filename.endswith(".txt"):
-                with open(os.path.join(clients_directory, filename), 'r') as f:
+                with open(os.path.join(directory_data, filename), 'r') as f:
                     client_name = f.readline().strip()
                 client_directories[client_name] = filename
 
         clients = {}
 
         for client_name, filename in client_directories.items():
-            client_name, client_port, client_password = read_client(os.path.join(clients_directory, filename))
+            client_name, client_port, client_password = read_client(os.path.join(directory_data, filename))
             clients[client_name] = obs.ReqClient(host='localhost', port=client_port, password=client_password)
 
         command_thread = Thread(target=command_listener, args=(clients,))
