b27b2e2c0043c0eec9e91a86384a82aa9f3e0036
v4u6h4n <v4u6h4ny@gmail.com>
2024-07-08 15:39:41 +1000

    Fixes to ffmpeg.

--- services/hyprland.sh
+++ services/hyprland.sh
@@ -158,16 +158,24 @@ loopback_start() {
 
         # ffmpeg -hwaccel vaapi -f v4l2 -framerate 60 -video_size 1920x1080 -input_format mjpeg -i $path_camera_desk_vaughan -pix_fmt yuv420p -f v4l2 /dev/video50 > /dev/null 2>&1 & disown
         # ffmpeg -f v4l2 -framerate 60 -video_size 1920x1080 -input_format mjpeg -i $path_camera_desk_vaughan -pix_fmt yuv420p -f v4l2 /dev/video50 > /dev/null 2>&1 & disown
+
         v4l2-ctl -d $path_camera_desk_vaughan -c focus_automatic_continuous=0
         ffmpeg -vaapi_device /dev/dri/renderD128 -f v4l2 -video_size 1920x1080 -framerate 60 -input_format mjpeg -i $path_camera_desk_vaughan \
             -i "/media/storage/Streaming/Video/flowers/flowers_looped.mp4" \
-            -filter_complex "[0:v]split=2[out2][out3]; \
-                                [out2]eq=gamma=1.0,colorkey=0x00FF00:0.3:0.2[ckout]; \
-                                [1:v][ckout]overlay[greenscreen]; \
-                                [out3]crop=1920:800:0:280[cropped]; \
-                                [greenscreen][cropped]overlay=0:280[out4]" \
-            -map "[out4]" -f v4l2 /dev/video50 \
+            -filter_complex "[0:v]eq=gamma=1.0, colorkey=0x00FF00:0.3:0.2[ckout];[1:v][ckout]overlay[out1]" \
+            -map "[out1]" -f v4l2 /dev/video50 \
             > /dev/null 2>&1 & disown
+
+        # v4l2-ctl -d $path_camera_desk_vaughan -c focus_automatic_continuous=0
+        # ffmpeg -vaapi_device /dev/dri/renderD128 -f v4l2 -video_size 1920x1080 -framerate 60 -input_format mjpeg -i $path_camera_desk_vaughan \
+        #     -i "/media/storage/Streaming/Video/flowers/flowers_looped.mp4" \
+        #     -filter_complex "[0:v]split=2[out2][out3]; \
+        #                         [out2]eq=gamma=1.0,colorkey=0x00FF00:0.3:0.2[ckout]; \
+        #                         [1:v][ckout]overlay[greenscreen]; \
+        #                         [out3]crop=1920:800:0:280[cropped]; \
+        #                         [greenscreen][cropped]overlay=0:280[out4]" \
+        #     -map "[out4]" -f v4l2 /dev/video50 \
+        #     > /dev/null 2>&1 & disown
 
     }
     loopback_start_desk_vaughan_player() {
