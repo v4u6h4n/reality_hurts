15d3f9b2fdd814ec290cf6b4cfa3c760e90ad073
v4u6h4n <v4u6h4ny@gmail.com>
2024-10-01 14:19:26 +1000

    fixes

--- configurator.sh
+++ configurator.sh
@@ -69,7 +69,7 @@ prerequisite() {
             # Headphones.
             output_device_headphones_1_name="Headset"
             output_device_headphones_1_script_name="headphones_1"
-            output_device_headphones_1_node_name="bluez_output.89:88:1F:FD:47:F7.1" # "bluez_output.74_2A_8A_40_AD_0E.1"
+            output_device_headphones_1_node_name="bluez_output.89_88_1F_FD_47_F7.1" # "bluez_output.74_2A_8A_40_AD_0E.1"
             output_device_headphones_1_address="89:88:1F:FD:47:F7" # headset: "74:2A:8A:40:AD:0E"
 
         # cameras
